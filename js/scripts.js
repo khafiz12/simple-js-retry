15bc5ccce7cf2408ddb6000030bef70bd82105f1
khafiz12 <karimhafiz7@gmail.com>
2023-11-06 23:06:05 -0500

    created pokemonlist array
    pokemonList array with 3 objects and keys.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+   
+   let pokemonList= [
+       {name: "pikachu", type: "thunder"},
+       {name: "balbasaur", type:["water","speed"]},
+       {name: "eve", type: "lightening"} 
+];
+
+console.log(pokemonList);
+
+
+
