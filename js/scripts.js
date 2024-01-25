3ee1ac8238ac2475b1b09358320dd4dd72d2025e
khafiz12 <karimhafiz7@gmail.com>
2024-01-24 20:21:40 -0500

    Appended pokemonList to button
    appended pokemonList to button. Still working on uploading pokemon img/type.

--- js/scripts.js
+++ js/scripts.js
@@ -21,16 +21,35 @@
         }
 
        function addListItem(pokemon){
-         let pokemonList = document.querySelector(".pokemon-list");
-         pokemonList.classList.add("list-item");
-         let listPokemon = document.createElement("li");
-         listPokemon.classList.add("list-group-item");
+        // let divElement= document.createElement("div");
+        // pokemonList.appendChild(divElement);
+        // divElement.classList.add("list-group-item','col-12','col-sm-6','col-md-4','col-lg-4");
+        // divElement.id=pokemon.name;
+
+        // let button= document.createElement("button");
+       //  button.innerText=pokemon.name;
+       //  button.setAttribute("data-id", "i++");
+       //  button.setAttribute("data-toggle", "modal");
+       //  button.setAttribute("data-target", "#exampleModal");  
+       //  divElement.appendChild(button);
+        // button.classList.add("btn","btn-lg","btn-block", "list-btn")
+
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        listPokemon.classList.add("list-group-item");
+        pokemonList.appendChild(listPokemon);
+               
+
+
+         //still need to clean up this code bc buttons are too close to side. FIGURE OUT WHAT DATA-ID, DATA-TARGET AND DATA-TOGGLE DO.//
          let button = document.createElement("button");
+         button.innerText=pokemon.name
+         button.setAttribute("data-id", "i++")
+         button.setAttribute("data-toggle", "modal");
+         button.setAttribute("data-target", "#exampleModal");
          button.classList.add("btn-primary")
          button.classList.add("btn-lg");
-         button.innerText = pokemon.name;
-         pokemonList.appendChild(listPokemon);
-         listPokemon.appendChild(button);
+         pokemonList.appendChild(button);
          button.addEventListener( "click", function () {
          showDetails(pokemon)
          }
@@ -43,7 +62,10 @@
          json.results.forEach(function(item){
          let pokemon= {name: item.name,
                        detailsUrl: item.url,
-                       height: item.height,}; 
+                       height: item.height,
+                       type: item.types,
+                       moves: item.moves, 
+                        }; 
           
          add(pokemon);
          });}).catch(function(e){console.error(e);})
@@ -55,7 +77,8 @@
          return response.json ();}).then(function(details){
          pokemon.height=  details.height;
          pokemon.imageUrl= details.sprites.front_default;
-         pokemon.type= details.type;
+         pokemon.type= details.types;
+         pokemon.moves= details.moves;
         }).catch(function(e){console.error(e)})
         }
 
@@ -65,25 +88,35 @@
         let modalTitle = document.querySelector(".modal-title");
         let modalHeader = document.querySelector(".modal-header");   
          
-        modalTitle.empty();
-        modalBody.empty();
-        modalHeader.empty();
+        // modalTitle.empty();
+        // modalBody.empty();
+        // modalHeader.empty();
         // mute modalContainer for now. test to see if task 1.10 work
-        //let modalContainer= document.querySelector("#modal-container"); 
-        //modalContainer.innerHTML=''
+       // let modalContainer= document.querySelector("#modal-container"); 
+       // modalContainer.innerHTML=''
 
         let titleElement=document.createElement("h1");
         titleElement.innerText= pokemon.name;
 
         let contentElement= document.createElement("p");
         contentElement.innerText= "Height:" + " "+ pokemon.height;
+        titleElement.appendChild(contentElement);
+
+        let detailsType=document.createElement("p");
+        detailsType.innerText= "Types:" + " " + pokemon.type;
+        //contentElement.appendChild(detailsType);
 
         let imageElement = document.createElement("img");
-        imageElement.src = pokemon.imageUrl; 
+        imageElement.src = pokemon.imageUrl;
+        imageElement.alt = pokemon.name;
+        //contentElement.appendChild(imageElement); 
+
         
         modalTitle.appendChild(titleElement);
         modalBody.appendChild(contentElement);
         modalBody.appendChild(imageElement);
+        modalBody.appendChild(detailsType);
+
         //need to add the body class missing in the HTML. maybe the reason why things arent working.//
         //modalContainer.appendChild(modalBody);
                     
